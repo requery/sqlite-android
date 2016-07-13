3d3d3960f6ee78aa241ac2b17431dcba8dad887d
Nikhil Purushe <npurushe@gmail.com>
2016-07-13 08:27:19 -0700

    Update selectionArgs to Object[]

--- sqlite-android/src/main/java/io/requery/android/database/sqlite/SQLiteDatabase.java
+++ sqlite-android/src/main/java/io/requery/android/database/sqlite/SQLiteDatabase.java
@@ -1254,7 +1254,7 @@ public final class SQLiteDatabase extends SQLiteClosable {
      * @see Cursor
      */
     public Cursor query(String table, String[] columns, String selection,
-            String[] selectionArgs, String groupBy, String having,
+            Object[] selectionArgs, String groupBy, String having,
             String orderBy, String limit) {
 
         return query(false, table, columns, selection, selectionArgs, groupBy,
