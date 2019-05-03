b42e4ea36f7a775f620612ad177a6e5309e1be67
Ben Asher <benasher44@gmail.com>
2019-05-03 16:09:37 -0700

    Update sqlite-android/src/main/java/io/requery/android/database/sqlite/SQLiteDatabase.java
    Co-Authored-By: EricAtPlanGrid <eric.oconnell@plangrid.com>

--- sqlite-android/src/main/java/io/requery/android/database/sqlite/SQLiteDatabase.java
+++ sqlite-android/src/main/java/io/requery/android/database/sqlite/SQLiteDatabase.java
@@ -826,7 +826,7 @@ public final class SQLiteDatabase extends SQLiteClosable implements SupportSQLit
         File file = new File(path);
         if (!file.exists()) {
             try {
-                if (!file.getParentFile().exists()) {
+                if (!file.getParentFile().exists() && !file.getParentFile().mkdirs()) {
                     // Fixes #103: Check parent directory's existence before
                     // attempting to create.
                     if (!file.getParentFile().mkdirs()) {
